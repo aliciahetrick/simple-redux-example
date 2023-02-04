163a868b7933fc2a88af9ff0e23c8081c43d6056
aliciahetrick <aliciahetrick@gmail.com>
2023-02-04 14:58:46 -0500

    Set up global state for counter

--- src/index.js
+++ src/index.js
@@ -1,17 +1,55 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//STORE aka global state
+import { createStore } from 'redux'
+// let store = createStore(reducer)
+
+//ACTION aka verb describing what you want to do
+//is a function that returns an object
+function increment() {
+  return {
+    type: 'INCREMENT',
+  }
+}
+
+function decrement() {
+  return {
+    type: 'DECREMENT',
+  }
+}
+
+//REDUCER aka describes how your actions transform state into the next state
+//is a function that returns an object
+function counter(state = 0, action) {
+  switch (action.type) {
+    case 'INCREMENT':
+      return state + 1
+    case 'DECREMENT':
+      return state - 1
+    default:
+      return state
+  }
+}
+
+let store = createStore(counter)
+
+//display it in the console:
+store.subscribe(() => {
+  console.log(store.getState())
+})
+
+//DISPATCH aka the way you execute your action
+store.dispatch(increment())
+
+const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-);
+)
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
