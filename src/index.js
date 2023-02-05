a387ec3a6f5105c130b8539bf51a78a82620077e
aliciahetrick <aliciahetrick@gmail.com>
2023-02-04 21:48:42 -0500

    Wrap app to have access to the store

--- src/index.js
+++ src/index.js
@@ -19,7 +19,9 @@ const store = createStore(
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider>
+      <App />
+    </Provider>
   </React.StrictMode>
 )
 
