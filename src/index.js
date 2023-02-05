7ea195a5a98176380ce970d45790df1e13ac3958
aliciahetrick <aliciahetrick@gmail.com>
2023-02-04 21:47:25 -0500

    Add reducer setup

--- src/index.js
+++ src/index.js
@@ -3,47 +3,18 @@ import ReactDOM from 'react-dom/client'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
-
-//STORE aka global state
 import { createStore } from 'redux'
-// let store = createStore(reducer)
+import rootReducer from './reducers'
 
-//ACTION aka verb describing what you want to do
-//is a function that returns an object
-function increment() {
-  return {
-    type: 'INCREMENT',
-  }
-}
+//connects state in store to react app
+import { Provider } from 'react-redux'
 
-function decrement() {
-  return {
-    type: 'DECREMENT',
-  }
-}
-
-//REDUCER aka describes how your actions transform state into the next state
-//is a function that returns an object
-function counter(state = 0, action) {
-  switch (action.type) {
-    case 'INCREMENT':
-      return state + 1
-    case 'DECREMENT':
-      return state - 1
-    default:
-      return state
-  }
-}
-
-let store = createStore(counter)
-
-//display it in the console:
-store.subscribe(() => {
-  console.log(store.getState())
-})
-
-//DISPATCH aka the way you execute your action
-store.dispatch(increment())
+//first param is combined reducer,
+//second param is the way to see in developer tools in chrome
+const store = createStore(
+  rootReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
