bb52b662eec93a07409f146bb297e528dafdb1cb
aliciahetrick <aliciahetrick@gmail.com>
2023-02-04 22:05:09 -0500

    Create and dispatch actions

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { increment, decrement } from './actions'
+
 function App() {
+  const counter = useSelector((state) => state.counter)
+  const isLogged = useSelector((state) => state.isLogged)
+  const dispatch = useDispatch()
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <h1>Counter:{counter} </h1>
+      <button onClick={() => dispatch(decrement())}>-</button>
+      <button onClick={() => dispatch(increment())}>+</button>
+      {isLogged ? <h3>Valuable info I shouldn't see</h3> : ''}
     </div>
   )
 }
